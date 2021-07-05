97bec4531c79bf86f245c852adb72b726c41b7e8
Ahmed Shafi Arnob <46849793+ShafiArnob@users.noreply.github.com>
2021-07-05 12:00:51 +0600

    static files

--- photos/urls.py
+++ photos/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path 
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.gallery, name='gallery'),
     path('photo/<str:pk>/', views.view_photo, name='photo'),
     path('add/',views.add_photo, name='add'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
