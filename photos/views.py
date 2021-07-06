75b143630b85fb9d2d68cee7dc7cda400051fd4e
Ahmed Shafi Arnob <46849793+ShafiArnob@users.noreply.github.com>
2021-07-06 23:09:26 +0600

    Add new image

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . models import Category, Photo
 # Create your views here.
 def gallery(request):
@@ -15,4 +15,29 @@ def view_photo(request,pk):
   return render(request,'photos/photo.html',{"photo":photo})
 
 def add_photo(request):
-  return render(request,'photos/add.html')
+  categories = Category.objects.all()
+
+  if request.method == "POST":
+    data = request.POST
+    # input 'name' is 'image'
+    image = request.FILES.get('image')
+
+    # if there is a category use the category
+    if data['category'] != 'none':
+      category = Category.objects.get(id=data['category'])
+    # if there is a new category use the new category 
+    elif data['category_new'] != '':
+      category, created = Category.objects.get_or_create(name=data['category_new'])
+    # if there is no category say None
+    else:
+      category = None
+
+    photo = Photo.objects.create(
+      category = category,
+      description = data['description'],
+      image = image,
+    )
+
+    return redirect('gallery')
+  context = {'categories':categories}
+  return render(request,'photos/add.html',context)
